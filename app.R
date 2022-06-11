77047913b415230285f694c9f5be3c0a848dafca
Relu Stătescu <statescu.relu@gmail.com>
2022-06-11 17:43:46 +0300

    description and binomial graphs

--- app.R
+++ app.R
@@ -14,12 +14,24 @@ ui <- fluidPage(
                            sliderInput(inputId = "bern", 
                                        label = "Probability of success",
                                        0, 1, 0.01),
+                           h5("The Bernoulli distribution is the discrete probability distribution
+                              of a random variable which takes the value 1 with probability
+                              p and the value 0 with probability q = 1 - p.The Bernoulli distribution
+                              is a special case of the binomial distribution where a single trial is
+                              conducted (so n would be 1 for such a binomial distribution).", style = "text-align: justify")
+                  
                          ),
                          conditionalPanel(
                            condition = "input.repartitie == 'bin'",
-                           sliderInput(inputId = "bin", 
-                                       label = "Probability of success ",
-                                       0, 1, 0.01)
+                           sliderInput(inputId = "binProb", 
+                                       label = "Probability of success",
+                                       0, 1, 0.01),
+                           numericInput(inputId = "binSize", 
+                                        label = "Nr. of events",
+                                        value = 10),
+                           h5("The binomial distribution with parameters n and p is the discrete probabilty distribution of the number of successes in a sequence of n independent experiments, each asking a yes-no question, and each with its own Boolean-valued outcome: success(with probability p) or failure(with probability q = 1 - p).",
+                              style = "text-align: justify")
+                           
                          )
                 ),
                 mainPanel(
@@ -50,29 +62,53 @@ ui <- fluidPage(
                                     )
                                   )
                                 )
-                                )
+                              )
                   )
                 ))
   
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
-    output$densityGraph <- renderPlot({
+  output$densityGraph <- renderPlot({
+    if (input$repartitie == "bern")
+    {
+      x <- seq(0, 3);
       prob <- input$bern
-      x <- seq(0, 5);
       density <- dbern(x, prob);
-      plot(density, type = "o");
-    });
+    }
+    
+    if (input$repartitie == "bin")
+    {
+      prob <- input$binProb;
+      size <- input$binSize;
+      density <- dbinom(0:size, size, prob);
+    }
+    plot(density, type = "o");
+  });
     output$distGraph <- renderPlot({
-      prob <- input$bern
-      x <- seq(0, 5);
-      distribution <- pbern(x, prob);
+      if (input$repartitie == "bern")
+      {
+        prob <- input$bern
+        x <- seq(0, 3);
+        distribution <- pbern(x, prob);
+      }
+      if (input$repartitie == "bin")
+      {
+        prob <- input$binProb;
+        size <- input$binSize;
+        distribution <- pbinom(0:size, size, prob);
+      }
       plot(distribution, type = "o");
     });
-    output$mean <- renderText({input$bern});
-    output$var <- renderText({input$bern * (1- input$bern)});
+    output$mean <- renderText({
+      if (input$repartitie == "bern") { input$bern }
+      else if (input$repartitie == "bin") { input$binProb * input$binSize }
+    });
+    output$var <- renderText({
+      if (input$repartitie == "bern") { input$bern * (1 - input$bern) }
+      else if (input$repartitie == "bin") { input$binProb * input$binSize * (1 - input$binProb) }
+    });
 
 }
 
